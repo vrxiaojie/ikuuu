543361e4deb44f79aa659734c102abcfe6e776df
vrxiaojie <wjj0227@126.com>
2025-02-22 13:48:26 +0800

    V1.0.1

--- app/checkin.py
+++ app/checkin.py
@@ -21,13 +21,9 @@ options.add_argument("--window-size=1920,1080")
 options.add_argument("--start-maximized")
 
 service = Service(executable_path='/usr/bin/msedgedriver')
-# 启动Edge浏览器
-driver = webdriver.Edge(service=service, options=options)
-traffic_before_signin = ""
-
 
 def open_url(url):
-    driver.set_page_load_timeout(5)
+    driver.set_page_load_timeout(2) # 设置页面超时2秒
     try:
         # 打开网页
         driver.get(url)  # 替换为目标网页的URL
@@ -35,12 +31,10 @@ def open_url(url):
         driver.execute_script("window.stop()")
 
 
-def get_usage() -> str:
+def get_usage() -> float:
     time.sleep(3)
-    elements = driver.find_elements(By.CLASS_NAME, 'card-body')
-    for item in elements:
-        if "GB" in item.text:
-            return item.text
+    traffic = driver.find_element(By.CSS_SELECTOR, '#app > div > div.main-content > section > div:nth-child(3) > div:nth-child(2) > div > div.card-wrap > div.card-body > span')
+    return float(traffic.text)
 
 
 def login():
@@ -48,53 +42,54 @@ def login():
     open_url('https://ikuuu.one/auth/login')
     WebDriverWait(driver, 10, poll_frequency=0.2).until(EC.presence_of_element_located((By.ID, 'email')))
     # 输入用户名
-    username_field = driver.find_element(By.ID, 'email')  # 假设用户名输入框的name属性是username
-    username_field.send_keys(username)  # 替换为你的用户名
+    username_field = driver.find_element(By.ID, 'email')
+    username_field.send_keys(username)
 
     # 输入密码
-    password_field = driver.find_element(By.ID, 'password')  # 假设密码输入框的name属性是password
-    password_field.send_keys(password)  # 替换为你的密码
+    password_field = driver.find_element(By.ID, 'password')
+    password_field.send_keys(password)
 
     # 提交登录表单
     password_field.send_keys(Keys.RETURN)  # 按下回车键提交
 
-    # 等待页面跳转
+    # 获取流量值
     global traffic_before_signin
     traffic_before_signin = get_usage()
-    print(f"登录成功，当前流量:{traffic_before_signin}")
+    print(f"登录成功，当前流量:{traffic_before_signin}GB")
 
 
 def sign_in():
-    WebDriverWait(driver, 10, poll_frequency=0.2).until(EC.presence_of_element_located((By.ID, 'checkin-div')))
+    WebDriverWait(driver, 10, poll_frequency=0.2).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#checkin-div')))
     # 点击签到按钮
-    sign_in_button = driver.find_element(By.ID, 'checkin-div')
+    sign_in_button = driver.find_element(By.CSS_SELECTOR, '#checkin-div')
     if '签到' in sign_in_button.text:
         sign_in_button.click()
         driver.execute_script("location.reload()")
-        gain_traffic = float(get_usage().split(' ')[0]) - float(traffic_before_signin.split(' ')[0])
+        gain_traffic = get_usage() - traffic_before_signin
         print(f"签到成功!签到获得流量:{gain_traffic} GB，")
     else:
-        print(f"今日已签到。当前流量{traffic_before_signin}")
+        print(f"今日已签到")
 
 
 def run():
+    global driver
     retry = 0
     while True:
         try:
+            driver = webdriver.Edge(service=service, options=options)
             login()
             sign_in()
             break
 
         except Exception as e:
             if retry == 3:
-                print("出现错误: {}".format(e))
+                print(f"出现错误: {e}")
                 break
             retry += 1
-            print(f"等待10s，第{retry}次重试")
-            time.sleep(10)
-
+            print(f"出现错误:{e} \n第{retry}次重试")
+            driver.quit()
+            time.sleep(2)
     driver.quit()
-
 
 if __name__ == '__main__':
     run()
--- ikuuu.py
+++ ikuuu.py
@@ -25,9 +25,6 @@ options.add_argument("--headless")  # 无界面模式
 options.add_argument("--disable-gpu")
 
 service = Service()
-# 启动Edge浏览器
-driver = webdriver.Edge(service=service, options=options)
-traffic_before_signin = ""
 
 def open_url(url):
     driver.set_page_load_timeout(2) # 设置页面超时2秒
@@ -38,12 +35,10 @@ def open_url(url):
         driver.execute_script("window.stop()")
 
 
-def get_usage() -> str:
+def get_usage() -> float:
     time.sleep(3)
-    elements = driver.find_elements(By.CLASS_NAME, 'card-body')
-    for item in elements:
-        if "GB" in item.text:
-            return item.text
+    traffic = driver.find_element(By.CSS_SELECTOR, '#app > div > div.main-content > section > div:nth-child(3) > div:nth-child(2) > div > div.card-wrap > div.card-body > span')
+    return float(traffic.text)
 
 
 def login():
@@ -51,12 +46,12 @@ def login():
     open_url('https://ikuuu.one/auth/login')
     WebDriverWait(driver, 10, poll_frequency=0.2).until(EC.presence_of_element_located((By.ID, 'email')))
     # 输入用户名
-    username_field = driver.find_element(By.ID, 'email')  # 假设用户名输入框的name属性是username
-    username_field.send_keys(username)  # 替换为你的用户名
+    username_field = driver.find_element(By.ID, 'email')
+    username_field.send_keys(username)
 
     # 输入密码
-    password_field = driver.find_element(By.ID, 'password')  # 假设密码输入框的name属性是password
-    password_field.send_keys(password)  # 替换为你的密码
+    password_field = driver.find_element(By.ID, 'password')
+    password_field.send_keys(password)
 
     # 提交登录表单
     password_field.send_keys(Keys.RETURN)  # 按下回车键提交
@@ -64,33 +59,41 @@ def login():
     # 获取流量值
     global traffic_before_signin
     traffic_before_signin = get_usage()
-    print(f"登录成功，当前流量:{traffic_before_signin}")
+    print(f"登录成功，当前流量:{traffic_before_signin}GB")
 
 
 def sign_in():
-    WebDriverWait(driver, 10, poll_frequency=0.2).until(EC.presence_of_element_located((By.ID, 'checkin-div')))
+    WebDriverWait(driver, 10, poll_frequency=0.2).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#checkin-div')))
     # 点击签到按钮
-    sign_in_button = driver.find_element(By.ID, 'checkin-div')
+    sign_in_button = driver.find_element(By.CSS_SELECTOR, '#checkin-div')
     if '签到' in sign_in_button.text:
         sign_in_button.click()
         driver.execute_script("location.reload()")
-        gain_traffic = float(get_usage().split(' ')[0]) - float(traffic_before_signin.split(' ')[0])
+        gain_traffic = get_usage() - traffic_before_signin
         print(f"签到成功!签到获得流量:{gain_traffic} GB，")
     else:
         print(f"今日已签到")
 
 
 def run():
-    try:
-        login()
-        sign_in()
+    global driver
+    retry = 0
+    while True:
+        try:
+            driver = webdriver.Edge(service=service, options=options)
+            login()
+            sign_in()
+            break
 
-    except Exception as e:
-        print("出现错误: {}".format(e))
-
-    finally:
-        driver.quit()
-
+        except Exception as e:
+            if retry == 3:
+                print(f"出现错误: {e}")
+                break
+            retry += 1
+            print(f"出现错误:{e} \n第{retry}次重试")
+            driver.quit()
+            time.sleep(2)
+    driver.quit()
 
 if __name__ == '__main__':
     run()
